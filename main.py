cda5f47fea23b291d3dee4fa33b5c001c5e3a6ed
ramya6016 <54950224+ramya6016@users.noreply.github.com>
2020-12-02 09:59:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import json
+import os
+import time
+import threading
+l=threading.Lock() #to make thread safe
+def create(key,value,ttl=0,new=0):
+    with l:
+        count=0
+        size=os.stat("values.txt").st_size
+        if (size < 1024 * 1024 * 1024): #to not exceed 1gb
+            if (len(key) < 32):
+                if (value <= 16 * 1024 * 1024):
+                    if(new==0):
+                        f=open("values.txt","r")
+                        data = f.read().split('\n')
+                        for line in data:
+                            line = line.split(' ')
+                            if (line[0] == key):
+                                print(key + " is already present")
+                                break
+                            else:
+                                count +=1
+                    elif(new == 1):
+                        f = open("values.txt", "w").close()
+                        data=[]
+                    temp=[]
+                    if (count == len(data)):
+                        if (ttl == 0):
+                            temp = [str(value), str(ttl)]
+                        elif (ttl > 0):
+                            temp = [str(value), str(time.time() + ttl)]
+                    if(new == 1):
+                        fr = open("values.txt", "w")
+                    elif(new == 0):
+                        fr = open("values.txt", "a")
+                    fr.write(key + " " + ' '.join(temp))
+                    fr.write('\n')
+                    fr.close()
+                else:
+                    print("value exceeded the memory limit")
+            else:
+                print("key is longer than expected")
+        else:
+            print("file size exceeded the memory limit")
+
+def destroy(key):
+    with l:
+        count=1
+        fs = open("values.txt", "r")
+        temp1 = fs.read().split('\n')
+        for i in range(len(temp1)):
+            temp = temp1[i]
+            data = (temp1[i].split(' '))
+            if (len(data) > 1):
+                if(data[0]==key):
+                    if(data[2] != '0'):
+                        if (time.time() >= float(data[2])):
+                            print("Time limit of " + key + " is expired!!")
+                    else:
+                        fw = open("values.txt", "w").close()
+                        for i in range(len(temp1)):
+                            data1 = (temp1[i].split(' '))
+                            if (len(data1) > 1):
+                                fr = open("values.txt", "a")
+                                if (data1[0] != key):
+                                    fr.write(' '.join(data1))
+                                    fr.write('\n')
+                                else:
+                                    continue
+                        print(key +" is Deleted successfully!")
+                else:
+                    count+=1
+        if(count==len(temp1)):
+            print(key+" does not exist in the datastore !!")
+
+def read(key):
+    with l:
+        count=1
+        fy = open("values.txt", "r")
+        temp1 = fy.read().split('\n')
+        for i in range(len(temp1)):
+            temp = temp1[i]
+            data = (temp1[i].split(' '))
+            if (len(data) > 1):
+                if(data[0]==key):
+                    if (data[2] != '0'):
+                        if (time.time() >= float(data[2])):
+                            print("Time limit of " + key + " is expired!!")
+                            break
+                    d={'key':key,'value':data[1]}
+                    print(d)
+                else:
+                    count=count+1
+        if(count==len(temp1)):
+            print(key + " is not present in the datastore!")
---
+++ test.py
@@ -0,0 +1,9 @@
+import main as x
+import time
+x.create('dog',8,10,1)
+x.create('cat',6)
+x.create('donkey',10)
+x.read('cat')
+x.destroy('donkey')
+time.sleep(10)
+x.read('dog')
